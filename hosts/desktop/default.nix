4aaac6a1dca40a630f75860fc60fd866df2b4791
Nickiel12 <nicholasyoungsumner@gmail.com>
2023-02-26 18:02:09 -0800

    fix: added nvidia drivers to try and fix screen tears

--- hosts/desktop/default.nix
+++ hosts/desktop/default.nix
@@ -1,11 +1,17 @@
-{ ...  }:
+{ config,  ... }:
 
 
 {
   imports = [
 	./hardware-configuration.nix
   ];
+  
+  services.xserver.videoDrivers = [ "nvidia" ];
+  hardware.opengl.enable = true;
 
+  # Optionally, you may need to select the appropriate driver version for your specific GPU.
+  hardware.nvidia.package = config.boot.kernelPackages.nvidiaPackages.stable;
+ 
   hardware.bluetooth.enable = true;
 
   time.hardwareClockInLocalTime = true;

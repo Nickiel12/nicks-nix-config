59246af22f8107ceefbb973574c2b3586a0f3f2e
Nickiel12 <nicholasyoungsumner@gmail.com>
2023-04-02 22:50:06 -0700

    Removed automatic betterdiscord script

--- modules/discord.nix
+++ modules/discord.nix
@@ -11,11 +11,11 @@
     ".config/BetterDiscord/themes/ClearVision_v6.theme.css".source = ../rsrcs/ClearVision_v6.theme.css;
   };
 
-  home.activation.discord = lib.hm.dag.entryAfter["writeBoundary"] ''
-    if [ ! -d .config/BetterDiscord/data ]; then
-      betterdiscordctl install
-    fi
-  '';
+  # home.activation.discord = lib.hm.dag.entryAfter["writeBoundary"] ''
+  #   if [ ! -d .config/BetterDiscord/data ]; then
+  #    betterdiscordctl install
+  #  fi
+  #'';
 
 
 }

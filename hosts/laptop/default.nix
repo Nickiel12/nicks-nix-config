8f04684b00cabc407ad480e771c3036196b9228a
Nickiel12 <nicholasyoungsumner@gmail.com>
2023-09-21 11:24:16 -0700

    the entry stopped showing up for some reason

--- hosts/laptop/default.nix
+++ hosts/laptop/default.nix
@@ -22,15 +22,19 @@
   boot.loader = { efi = {
       canTouchEfiVariables = true; efiSysMountPoint = "/boot";
     };
-    grub = { devices = [ "nodev" ]; efiSupport = true; enable = true; 
-      useOSProber = false; extraEntries = ''
-menuentry 'Windows Boot Manager (on /dev/nvme0n1p1)' --class windows 
-	--class os $menuentry_id_option 'osprober-efi-364F-BE7A' { 
-	insmod part_gpt insmod fat search --no-floppy --fs-uuid 
-	--set=root 364F-BE7A chainloader 
-	/efi/Microsoft/Boot/bootmgfw.efi
-}
-      '';
+    grub = {
+      devices = [ "nodev" ];
+      efiSupport = true;
+      enable = true; 
+      useOSProber = true;
+      # extraEntries = ''
+# menuentry 'Windows Boot Manager (on /dev/nvme0n1p1)' --class windows 
+	# --class os $menuentry_id_option 'osprober-efi-364F-BE7A' { 
+	# insmod part_gpt insmod fat search --no-floppy --fs-uuid 
+	# --set=root 364F-BE7A chainloader 
+	# /efi/Microsoft/Boot/bootmgfw.efi
+# }
+      # '';
     };  
   };
 

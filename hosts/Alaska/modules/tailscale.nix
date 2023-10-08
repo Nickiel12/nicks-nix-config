fd76980110aabdd0dadc0baa378bf987cffde25f
Nickiel12 <nicholasyoungsumner@gmail.com>
2023-10-08 12:57:47 -0700

    added important note to setup instructions

--- hosts/Alaska/modules/tailscale.nix
+++ hosts/Alaska/modules/tailscale.nix
@@ -3,9 +3,10 @@
 {
   # to add a new headscale "user", run this command
   # sudo headscale users create elnuhub
-  # then run this command where headscale_url is the http path 
+  # then run this command where headscale_url is the https path 
   # to the server
-  # tailscale up --login-server <headscale_url>
+  ## NOTE: Must Include the HTTPS://
+  # tailscale up --login-server <headscale_url> 
   # then replace USERNAME with the computer's hostname which you created
   # an "user" account in the first comment
 

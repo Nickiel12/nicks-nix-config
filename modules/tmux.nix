9b286332cecbc0434c24395b54641ce1d3ebde58
Nickiel12 <nicholasyoungsumner@gmail.com>
2023-10-14 11:30:58 -0700

    added vim-like bindings

--- modules/tmux.nix
+++ modules/tmux.nix
@@ -12,5 +12,22 @@ in
     keyMode = "vi";
     mouse = true;
     shell = "${pkgs.zsh}/bin/zsh";
+    shortcut = "q";
+    escapeTime = 0;
+    extraConfig = ''
+      bind -T copy-mode-vi v send-keys -X begin-selection
+      bind -T copy-mode-vi y send-keys -X copy-pipe-and-cancel 'xclip -in -selection clipboard'
+
+      # vim-like pane switching
+      bind -r ^ last-window
+      bind -r k select-pane -U
+      bind -r j select-pane -D
+      bind -r h select-pane -L
+      bind -r l select-pane -R
+
+      # my-neovim-like panel splitting
+      bind -r v split-window -h
+      bind -r s split-window -v
+    '';
   };
 } 

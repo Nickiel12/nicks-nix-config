44b6333bd901ded525fc16b1129426d496dbec91
Nickiel12 <nicholasyoungsumner@gmail.com>
2023-12-18 21:25:25 -0800

    added go support; trimmed unused plugins

--- modules/neovim.nix
+++ modules/neovim.nix
@@ -137,7 +137,6 @@ in
         key = "<leader>d";
         action = "\"_d";
       }
-
       {
         # leader+p delete the selection to the void, then paste
         mode = "x";
@@ -148,10 +147,16 @@ in
 
     plugins = {
 
+      lsp.enable = true;
+
+      lsp.servers = {
+        gopls.enable = true;
+      };
+
       nvim-tree = {
         enable = true;
         openOnSetup = true;
-      };
+      }; 
 
       nvim-autopairs = {
         enable = true;
@@ -166,15 +171,10 @@ in
           "rust"
           "toml"
           "lua"
+          "go"
         ];
       };
       rainbow-delimiters.enable = true;
-      treesitter-refactor = {
-        enable = true;
-        #highlightCurrentScope.enable = true;
-        navigation.enable = true;
-        smartRename.enable = true;
-      };
 
       comment-nvim = {
         enable = true;
@@ -200,9 +200,11 @@ in
       # Read settings  here: https://github.com/mfussenegger/nvim-dap#Usage
       # See :help dap.txt, :help dap-mapping and :help dap-api.
       cmp-dap.enable = true;
-      cmp-buffer.enable = true;
+      # method signature with current arguement hints)
       cmp-nvim-lsp-signature-help.enable = true;
+      # lua source autocomplete
       cmp-nvim-lua.enable = true;
+      # filesystem paths
       cmp-path.enable = true;
 
       presence-nvim.enable = true;

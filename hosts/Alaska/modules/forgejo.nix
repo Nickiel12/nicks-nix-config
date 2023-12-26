f87dc5734b1dd0cedc312478c943d9448b58da2f
Nickiel12 <nicholasyoungsumner@gmail.com>
2023-12-26 09:02:02 -0800

    migrated gitea breaking change

--- hosts/Alaska/modules/forgejo.nix
+++ hosts/Alaska/modules/forgejo.nix
@@ -8,7 +8,7 @@ let
 in
 {
   system.activationScripts.copyStuff = "cp -r ${../../../rsrcs/giteaCustomDir}/. /Aurora/Forgejo/custom";
-  system.activationScripts.copyTheme = "cp ${palenight}/dist/theme-palenight.css /Aurora/Forgejo/custom/public/css/";
+  system.activationScripts.copyTheme = "cp ${palenight}/dist/theme-palenight.css /Aurora/Forgejo/custom/public/assets/css/";
 
   services.gitea = {
     enable = true;

a5e9770cea7afad4417c68fd1d028640c3b2573f
Nickiel12 <nicholasyoungsumner@gmail.com>
2024-01-09 19:27:51 -0800

    re-add atuin proxypass

--- hosts/Alaska/modules/atuin.nix
+++ hosts/Alaska/modules/atuin.nix
@@ -19,7 +19,7 @@ in
       forceSSL = true;
       enableACME = true;
       locations."/" = {
-        # proxyPass = "http://127.0.0.1:${builtins.toString config.services.atuin.port}";
+        proxyPass = "http://127.0.0.1:${builtins.toString config.services.atuin.port}";
         proxyWebsockets = true;
         extraConfig = ''
           allow 100.64.0.0/16;

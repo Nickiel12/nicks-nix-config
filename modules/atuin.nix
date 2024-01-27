a0777382e63e94e60878e445c3f3a98467eb0bff
Nickiel12 <nicholasyoungsumner@gmail.com>
2024-01-27 10:31:55 -0800

    changed default search

--- modules/atuin.nix
+++ modules/atuin.nix
@@ -16,7 +16,7 @@ in
       sync_frequency = "20m";
       sync_address = if (hostname == "Alaska") then "http://127.0.0.1:8910"
       else "https://atuin.nickiel.net";
-      filter_mode = "session"; #"global" | "host" | "session" | "directory"
+      filter_mode = "host"; #"global" | "host" | "session" | "directory"
       inline_height = 10;
       show_preview = true;
       show_help = true;

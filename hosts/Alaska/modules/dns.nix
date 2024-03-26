0900f1b41a7847fcf21eb229f4278889cb119687
Nickiel12 <nickiel@nickiel.net>
2024-03-25 17:05:00 -0700

    added intrim headscale make-to fix

--- hosts/Alaska/modules/dns.nix
+++ hosts/Alaska/modules/dns.nix
@@ -2,26 +2,26 @@
     {
       name = "vaultwarden.nickiel.net";
       type = "A";
-      value = "100.64.0.1";
+      value = "100.64.0.1/31";
     }
     {
       name = "git.nickiel.net";
       type = "A";
-      value = "100.64.0.1";
+      value = "100.64.0.1/31";
     }
     {
       name = "files.nickiel.net";
       type = "A";
-      value = "100.64.0.1";
+      value = "100.64.0.1/31";
     }
     {
       name = "iot.nickiel.net";
       type = "A";
-      value = "100.64.0.1";
+      value = "100.64.0.1/31";
     }
     {
       name = "atuin.nickiel.net";
       type = "A";
-      value = "100.64.0.1";
+      value = "100.64.0.1/31";
     }
   ]

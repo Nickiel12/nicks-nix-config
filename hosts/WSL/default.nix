ab4faa2f7e127cca18ebdb2ffcc89e1d25178cfa
Nickiel12 <nickiel@nickiel.net>
2024-07-12 02:00:26 +0000

    added postgres to wsl installation for dev server

--- hosts/WSL/default.nix
+++ hosts/WSL/default.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, ... }:
+{ config, lib, pkgs, ... }:
 
 {
   # See https://nix-community.github.io/NixOS-WSL/how-to/change-username.html
@@ -30,5 +30,17 @@
     };
     package = pkgs.nixVersions.stable;
     extraOptions = "experimental-features = nix-command flakes";
+  };
+
+  services.postgresql = {
+    enable = true;
+    enableTCPIP = true;
+    authentication = lib.mkForce ''
+      # TYPE  DATABASE        USER            ADDRESS                 METHOD
+      local   all             all                                     trust
+      host    all             all             127.0.0.1/32            trust
+      host    all             all             ::1/128                 trust
+      host    all             all        0.0.0.0/0     md5
+      ''; # address is the computer you are connecting from
   };
 }

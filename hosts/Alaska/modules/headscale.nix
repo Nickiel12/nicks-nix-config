5b5802e5290a7d70c2e7e2640aac9449b022087f
Nickiel12 <nickiel@nickiel.net>
2025-06-11 16:37:05 -0700

    removed headscale to migrate to 26.1

--- hosts/Alaska/modules/headscale.nix
+++ hosts/Alaska/modules/headscale.nix
@@ -1,4 +1,4 @@
-{  config, pkgs, headscale, ... }:
+{  config, pkgs, ... }:
 
 let
   tailscale_dns_entries = import ./dns.nix;
@@ -16,7 +16,6 @@ in {
 
   # https://carjorvaz.com/posts/setting-up-headscale-on-nixos/
   services.headscale = {
-    package = headscale.packages.${pkgs.system}.headscale;
     enable = true;
     address = "0.0.0.0";
     port = 8082;

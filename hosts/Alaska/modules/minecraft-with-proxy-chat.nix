651a73c74f58c2e5186091c2c4d40a094df5caa3
Nickiel12 <nickiel@nickiel.net>
2025-11-19 19:11:12 -0800

    added a backup service for the mincraft world

--- hosts/Alaska/modules/minecraft-with-proxy-chat.nix
+++ hosts/Alaska/modules/minecraft-with-proxy-chat.nix
@@ -22,7 +22,20 @@ in
         MODRINTH_PROJECTS = "fabric-api,simple-voice-chat:fabric-1.21.10-2.6.5,lithium:mc1.21.10-0.20.0-fabric,ferrite-core:CtMpt7Jr,death_coordinates:d4RzEbQZ";
       };
       volumes = [
-        "/home/minecraft:/data"
+        "/home/nixolas/minecraft:/data"
+      ];
+    };
+    minecraft-backups = {
+      autoStart = true;
+      image = "itzg:docker-mc-backup";
+      environment = {
+        BACKUP_INTERVAL = "24h";
+        BACKUP_ON_STARTUP = "false";
+        INITIAL_DELAY = "5h";
+      };
+      volumes = [
+        "/home/nixolas/minecraft:/data:ro"
+        "/Aurora/Backups/Minecraft/backups"
       ];
     };
   };

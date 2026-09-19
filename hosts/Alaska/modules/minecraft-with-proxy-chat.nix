2656b6e81f0a9353f6f5b2194c5e2e5d3faede57
Nickiel12 <nickiel@nickiel.net>
2026-09-19 14:36:07 -0700

    updated to 26.2

--- hosts/Alaska/modules/minecraft-with-proxy-chat.nix
+++ hosts/Alaska/modules/minecraft-with-proxy-chat.nix
@@ -16,11 +16,11 @@ in
       environment = {
         EULA = "TRUE";
         TYPE = "FABRIC";
-        VERSION = "1.21.11";
+        VERSION = "26.2";
         MEMORY = "3G";
         ENABLE_WHITELIST = "TRUE";
         WHITELIST = "Nickiel,SneakyLilBean,MadamePotaytoe,blueesskyyy,Ahanagod,backrapier25";
-        MODRINTH_PROJECTS = "fabric-api,simple-voice-chat:fabric-1.21.11-2.6.11,lithium:mc1.21.11-0.21.2-fabric,ferrite-core:8.0.3-fabric,death_coordinates:m1I6Qblh,disconnect-packet-fix:2.0.0-fabric";
+        MODRINTH_PROJECTS = "fabric-api:0.160.0+26.2,simple-voice-chat:fabric-2.6.23+26.2,lithium:mc26.2-0.25.3-fabric,ferrite-core:9.0.0-fabric,death_coordinates:m1I6Qblh,disconnect-packet-fix:2.2.0-fabric";
       };
       volumes = [
         "/home/nixolas/minecraft:/data"
